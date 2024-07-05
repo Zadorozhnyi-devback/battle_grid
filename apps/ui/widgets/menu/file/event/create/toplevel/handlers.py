f6e0f177f5d08a7f5c8d1b5a3217d5a3913d151b
Monster <BreakMonster95@gmail.com>
2024-07-05 20:36:19 +0300

    refactor open_event func

--- apps/ui/widgets/menu/file/event/create/toplevel/handlers.py
+++ apps/ui/widgets/menu/file/event/create/toplevel/handlers.py
@@ -1,5 +1,6 @@
 from tkinter import messagebox
 
+from app.settings.ui.fields.category_toplevel import CATEGORY_TOPLEVEL_FIELDS
 from app.settings.ui.fields.main import MAIN_FIELDS
 from apps.ui.handlers.cleaners import destroy_if_exists
 
@@ -13,7 +14,10 @@ def create_new_event(self) -> None:
     if messagebox.askyesno(
         message='Create new event and close current window?'
     ) is True:
-        destroy_if_exists(self, fields=MAIN_FIELDS)
+        destroy_if_exists(
+            self,
+            fields=[*MAIN_FIELDS, *CATEGORY_TOPLEVEL_FIELDS]
+        )
         self.__init__()
     else:
         self._window.focus_force()
